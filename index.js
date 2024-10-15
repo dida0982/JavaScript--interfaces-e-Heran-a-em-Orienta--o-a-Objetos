0a44e7e78300e131377894204727f24598806aee
Guilherme Barros JR <dida0982@gmail.com>
2024-10-15 11:26:08 -0300

    ...

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ import { Director } from "./Employees/Director.js";
 import { AuthenticationSystem } from "./AuthenticationSystem.js";
 
 const director = new Director("Rodrigo", 10000, 1321658413);
-const manager = new Manager("Rodrigo", 10000, 1321658413);
+director.RegisterPassword("123456");
 
-const loggedIn = AuthenticationSystem.login(director, "123456789");
+const manager = new Manager("Rodrigo", 5000, 1321658413);
+manager.registerPassword("123");
 
-console.log(loggedIn);
+const customer = new Customer("Lais", 52316854654, "456");
+
+const managerLoggedIn = AuthenticationSystem.login(manager, "123");
+const directorLoggedIn = AuthenticationSystem.login(director, "123456");
+
+const customerLoggedIn = AuthenticationSystem.login(customer, "456");
+
+console.log(managerLoggedIn, directorLoggedIn, customerLoggedIn);
+
+
